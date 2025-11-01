572d66f118ed934ea5efef07547c3f7836936681
andre monteiro <124000838+AndreMonteiro225@users.noreply.github.com>
2025-11-01 13:59:58 -0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,6 +44,8 @@ export const viewport = {
   initialScale: 1,
 }
 
+import SetVhClient from "./components/SetVhClient"; // <--- adicionado
+
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
@@ -51,6 +53,7 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
       </head>
       <body className={`${mainFontFamily.className}`}>
+        <SetVhClient /> {/* <-- adicionado */}
         <Header />
         <main>
           {children}
